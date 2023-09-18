09772320e16850497d3a8a720a8ff68ecae35ef0
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2023-09-18 12:26:24 +0530

    Static method

---
+++ Display.java
@@ -0,0 +1,9 @@
+public class Display {
+    public static void main(String[] args) {
+        show();
+    }
+    public static void show(){
+        System.out.println("We can create static method by using static keyword. We can call the method without creating object with this keyword.");
+    }
+}
+
